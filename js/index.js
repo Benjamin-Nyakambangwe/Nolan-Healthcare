7a89dea19ab1df0503521d217d331e3818c7f305
Benjamin Nyakambangwe <benjaminnyakambangwe@gmail.com>
2020-09-03 20:05:36 +0200

    added the drop nav

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
+const openNavLink = document.getElementById('open-nav-link');
+const nav = document.getElementById('navbar');
+
+
+openNavLink.addEventListener('click', openNav);
+
+
+//Open Nav on mobile
+function openNav() {
+  console.log('Hamburger Woking');
+
+  if (nav.style.display != 'none') {
+    nav.style.display = 'none';
+  } else {
+    nav.style.display = 'block';
+    console.log('else working');
+  }
+}
+
+
 AOS.init();
 
     $(document).ready(function(){
